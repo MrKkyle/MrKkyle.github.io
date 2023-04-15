0ef9bbe7aa3ec29685e657bf8ef69969c8cf9a5d
MrKkyle <Kyle.apocrypha@gmail.com>
2023-04-15 21:55:06 +0200

    updated

--- Scripts/Script4.js
+++ Scripts/Script4.js
@@ -1,15 +1,6 @@
 //Get the modal
 var modal = document.getElementById('id01');
 
-//When the user clicks anywhere outside of the modal , close it 
-window.onclick = function(event)
-{
-	if(event.target == modal)
-	{
-		modal.style.display = "none";
-	}
-}
-
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() 
